5d487ff30cdd8383ca03885d5222938f2c431b5d
jefferytheo <omor.jeffery@gmail.com>
2024-07-18 12:27:00 +0100

    mock window alert

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,18 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
+// Mock window.alert
+window.alert = jest.fn();
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
